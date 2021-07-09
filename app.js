81bae14dbdd9f94f4802d775dcffa26df59795a5
alcohol-korea <han071416@naver.com>
2021-07-09 15:46:52 +0900

    2.6 object
    property , property updating , adding property

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ console.log(loggenIn);//null 값
 let a;//값이 없음 
 console.log(a);//undefnied 타입 변수는 인지하지만 변수에대한 값이 없다
 
+//variable
+const playerName = "nico";//player property
+const playerHandsome = true;
+//아래꺼가 훨씬 더 정돈되어있음
+//player.name;
+//player.handsome;
+//array
+// player = ["nico",true];//이것은 어떤 property가 어떤 값인지 모름
+//object
+const player = {
+    name : "nico",
+    handsome : true, 
+}; //우리는 객체에서 =을:로 쓰고 ;를 ,로 쓴다
+console.log(player.name);//nico
+console.log(player["handsome"]);//["property"]로 작성해야 된댜.  true
+//갱신하기
+//player = false;// error 왜냐면 const이기에 갱신 불가능
+player.handsome = false;
+//property 추가
+player.age = 30;
+console.log(player.handsome);//false
+console.log(player.age);//30
+//upadating with adding
+//나이를 여기서 더 추가하고 싶다면 바꾸는것도 있지만 
+player.age = player.age + 15;//도 있다.
+console.log(player.age);//45
