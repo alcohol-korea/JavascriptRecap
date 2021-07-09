18909a6adbeea94755feb5d82ef6c4033c901d7a
alcohol-korea <han071416@naver.com>
2021-07-09 13:26:35 +0900

    2.2 Variables
    variable은 값을 저장하거나 유지하는 역할을 함, variable은 공백이 없으므로 글자가 달라지는 지점에서 대문자를 써서 이어나감 ex) veryLongVariables 그리고 이것을 camelcase라함

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const a = 10; 
+const b = 2;
+const veryLongVariable = "hi" ; //camelcase라함
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+//variable은 값을 저장하거나 유지하는 역할을 함
+//변수는 공백이 없으므로 글자가 달라지는 지점에서 대문자를 써야ㅐ함
+console.log("hello"+veryLongVariable);
+
