3a6f072eec3750f9a07be4026f8ddad4ac7efe04
alcohol-korea <han071416@naver.com>
2021-07-23 19:43:06 +0900

    #5.2 padStart
    조건부 연산자를 String과 padStart를 이용하여 더 코드를 간략히 함

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,17 @@
-const clockTitle = document.querySelector(".js-clock");
+const clock = document.querySelector(".js-clock");
  
 function getTime(){
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    return clockTitle.innerText = `${hours<10 ? `0${hours}` : `${hours}`}:${minutes<10 ? `0${minutes}` : `${minutes}`}:${seconds<10 ? `0${seconds}` : `${seconds}`}`;
+    const hours = String(now.getHours()).padStart(2,"0");
+		// now.get시간은 숫자 문자열로 바꾸고 padStart함
+    const minutes = String(now.getMinutes()).padStart(2,"0");
+    const seconds = String(now.getSeconds()).padStart(2,"0");
+		clock.innerText = `${hours}:${minutes}:${seconds}`;
+
 }
 
 function init(){
 getTime();
 setInterval(getTime ,1000 );
 }
-init();
+init(); 
