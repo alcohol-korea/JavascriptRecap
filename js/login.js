a5148587738c79664ca33ec79db0387f1b68db45
alcohol-korea <han071416@naver.com>
2021-07-25 15:35:32 +0900

    #4.1 HTML JS로 username.value 관리

--- js/login.js
+++ js/login.js
@@ -2,7 +2,15 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function loginButtonClickOn(e){
-    console.log("hello",loginInput.value);
+    const username = loginInput.value;
+    if(username === ""){
+        alert("you have to write your name");
+        return;
+    }else if(username.length>8){
+        alert("name is too long");
+        return;
+    }
+    return console.log("hello",username);
 }
 
 loginButton.addEventListener("click",loginButtonClickOn);
