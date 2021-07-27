4f45cefad12f691ea4800c0043095892d024cde9
alcohol-korea <han071416@naver.com>
2021-07-27 15:22:38 +0900

    #4.5 LocalStorage
    localstorage method
    setItem: 정보 저장, getItem은 정보 가져오기, removeItem은 정보 삭제, clear은 다삭제

--- js/login.js
+++ js/login.js
@@ -3,10 +3,12 @@ const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CN = "hidden";
+const USERNAME_LS = "username";
 
 function loginSubmitOn(e){
     e.preventDefault();//addEventListener의 첫번쨰 인자에 있는 함수이며 이벤트 동작을 막는다
     const username = loginInput.value; //변수에 저장
+    localStorage.setItem(USERNAME_LS,username);
     loginForm.classList.add(HIDDEN_CN);//form없애기
     greeting.innerText = `Hello ${username}`;//h1에 이름 넣기
     greeting.classList.remove(HIDDEN_CN);//h1의 hidden class제거
