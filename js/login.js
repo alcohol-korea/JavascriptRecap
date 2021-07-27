4d5aa88927900dc15b51a352173b5fa50c14c8d4
alcohol-korea <han071416@naver.com>
2021-07-27 14:22:53 +0900

    #4.4 Greeting Username
    hidden class를 추가 삭제와 innerText에서 backtick과 그안에 변수는 ${}|안에 넣는것

--- js/login.js
+++ js/login.js
@@ -1,18 +1,16 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting")
 
-function loginSubmitOn(info){
-    info.preventDefault();//addEventListener의 첫번쨰 인자에 있는 함수이며 이벤트 동작을 막는다
-    console.log(info);
-}
-
-function handleClicked(event){
-    event.preventDefault();
-    console.log(event);
+function loginSubmitOn(e){
+    e.preventDefault();//addEventListener의 첫번쨰 인자에 있는 함수이며 이벤트 동작을 막는다
+    const username = loginInput.value; //변수에 저장
+    loginForm.classList.add("hidden");//form없애기
+    greeting.innerText = `Hello ${username}`;//h1에 이름 넣기
+    greeting.classList.remove("hidden");//h1의 hidden class제거
 }
 
 loginForm.addEventListener("submit",loginSubmitOn);//submit이벤트는 form만 쓸수 있는 이벤트이다 
-link.addEventListener("click",handleClicked);
+
 
 
