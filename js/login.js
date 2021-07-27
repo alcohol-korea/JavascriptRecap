3160d399d55d3eecc7a14409666128567d2b8a31
alcohol-korea <han071416@naver.com>
2021-07-27 14:38:15 +0900

    대문자 변수
    문자열만 저장하는 변수임

--- js/login.js
+++ js/login.js
@@ -2,12 +2,14 @@ const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CN = "hidden";
+
 function loginSubmitOn(e){
     e.preventDefault();//addEventListener의 첫번쨰 인자에 있는 함수이며 이벤트 동작을 막는다
     const username = loginInput.value; //변수에 저장
-    loginForm.classList.add("hidden");//form없애기
+    loginForm.classList.add(HIDDEN_CN);//form없애기
     greeting.innerText = `Hello ${username}`;//h1에 이름 넣기
-    greeting.classList.remove("hidden");//h1의 hidden class제거
+    greeting.classList.remove(HIDDEN_CN);//h1의 hidden class제거
 }
 
 loginForm.addEventListener("submit",loginSubmitOn);//submit이벤트는 form만 쓸수 있는 이벤트이다 
